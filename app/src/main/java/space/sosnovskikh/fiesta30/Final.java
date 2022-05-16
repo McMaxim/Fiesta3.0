11996f17d82a0a7bcf6292511d87e1adf2142c78
Sosnovskikh <maximsosnovskikh@gmail.com>
2022-05-16 19:11:30 +0300

    end

--- app/src/main/java/space/sosnovskikh/fiesta30/Final.java
+++ app/src/main/java/space/sosnovskikh/fiesta30/Final.java
@@ -12,21 +12,36 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Final extends AppCompatActivity implements View.OnClickListener {
-    TextView textView;
-
+    TextView textView,text;
+    MyDB database;
     ImageButton buttonh;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.final_1);
-
+        text = findViewById(R.id.textView6);
         textView = findViewById(R.id.textView7);
         buttonh =findViewById(R.id.button2);
         buttonh.setOnClickListener( this);
         textView.setOnClickListener( this);
 
         textView.setText(String.valueOf(Osnova.otvet)+"/5");
-
+        database = new MyDB(this);
+        if (database.selectAll().size() == 0) {
+            database.insert(0, 0);
+        }
+        Table table = database.select(1);
+        int point = table.getPoint(), stat = table.getStat();
+        text.setText(String.valueOf(point) + ", " + String.valueOf(stat));
+        Table table1 = new Table();
+        table1.setId(1);
+        table1.setStat(1);
+        table1.setPoint(Osnova.otvet);
+        database.update(table1);
+        Table table2 = database.select(1);
+        point = table2.getPoint();
+        stat = table2.getStat();
+        text.setText(String.valueOf(point) + ", " + String.valueOf(stat));
         if (MainActivity.ump== 0) {
             play();
         }
