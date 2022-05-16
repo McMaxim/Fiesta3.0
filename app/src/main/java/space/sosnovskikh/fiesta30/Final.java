92c10ad9806f596b82a1bd459fbdd6043df070de
Sosnovskikh <maximsosnovskikh@gmail.com>
2022-05-16 20:26:24 +0300

    ending

--- app/src/main/java/space/sosnovskikh/fiesta30/Final.java
+++ app/src/main/java/space/sosnovskikh/fiesta30/Final.java
@@ -3,6 +3,7 @@ package space.sosnovskikh.fiesta30;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -28,20 +29,34 @@ public class Final extends AppCompatActivity implements View.OnClickListener {
         textView.setText(String.valueOf(Osnova.otvet)+"/5");
         database = new MyDB(this);
         if (database.selectAll().size() == 0) {
-            database.insert(0, 0);
+            database.insert(0, 0 ,0);
         }
+        Log.d("TAG", "onCreate: ");
         Table table = database.select(1);
-        int point = table.getPoint(), stat = table.getStat();
-        text.setText(String.valueOf(point) + ", " + String.valueOf(stat));
+        Log.d("TAG", "onCreate:1 ");
+        int point = table.getPoint(), stat = table.getStat(), number = table.getNumber();
+        Log.d("TAG", "onCreate:2 ");
+        text.setText(String.valueOf(point) + ", " + String.valueOf(stat) +", " + String.valueOf(number));
+        Log.d("TAG", "onCreate: 3");
         Table table1 = new Table();
+        int a;
+        Log.d("TAG", "onCreate: 4");
+        a = database.select(1).getNumber()+1;
+        Log.d("TAG", "onCreate: 5");
         table1.setId(1);
-        table1.setStat(1);
+
         table1.setPoint(Osnova.otvet);
+        Log.d("TAG", "onCreate: 6");
+        table1.setNumber(a);
+        Log.d("TAG", "onCreate: 7");
+        table1.setStat((0+database.select(1).getStat()+database.select(1).getPoint())/a);
+        Log.d("TAG", "onCreate: 8");
         database.update(table1);
         Table table2 = database.select(1);
         point = table2.getPoint();
         stat = table2.getStat();
-        text.setText(String.valueOf(point) + ", " + String.valueOf(stat));
+        number = table2.getNumber();
+        text.setText(String.valueOf(point) + ", " + String.valueOf(stat) +", " + String.valueOf(number));
         if (MainActivity.ump== 0) {
             play();
         }
